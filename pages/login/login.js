84b62f7cef84972ac68b932eb730d36bb70fa052
Bohemian-hub <dongshangwl@gmail.com>
2020-11-20 23:12:45 +0800

    登录页面的布局

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-08 23:33:47
+ * @LastEditTime: 2020-11-20 09:52:22
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /miniprogram-5/pages/login/login.js
+ */
 // pages/login/login.js
 Page({
 
@@ -7,28 +15,35 @@ Page({
   data: {
 
   },
-
-  login() {
-
+  loginForm: function (data) {
+    console.log(data.detail.value)//  {username: "hgj", password: "fsdfsd"}
+    var username = data.detail.value.username;
+    var password = data.detail.value.password;
     wx.request({
-      url: 'http://127.0.0.1:8000/info/schedule',
-      header:{
+      url: 'http://127.0.0.1:8000/info/pinfo',
+      header: {
         "content-type": "application/x-www-form-urlencoded"		//使用POST方法要带上这个header
       },
       method: "POST",
-      data:{
-        name: "1811030132",
-        pwd:"dswl1234"
+      data: {
+        name: username,
+        pwd: password,
       }, // 向后端发送的数据，后端通过request.data拿到该数据
       success: res => {
         if (res.statusCode == 200) {
-            console.log(res);
+          console.log(res);
         }
       }
     })
-  
+  },
+
+  login() {
+
+
+
 
   },
+
   /**
    * 生命周期函数--监听页面加载
    */
