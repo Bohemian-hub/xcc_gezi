2a6cef8c1ff503b195cb7fb86b0238cb1d9ad33c
Bohemian-hub <dongshangwl@gmail.com>
2020-12-06 17:58:32 +0800

    正在完成代取员审核页面

--- pages/express_catch/express_catch.js
+++ pages/express_catch/express_catch.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-06 15:05:19
- * @LastEditTime: 2020-12-06 17:31:53
+ * @LastEditTime: 2020-12-06 17:48:52
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /miniprogram-5/pages/express_catch/express_catch.js
@@ -21,7 +21,28 @@ Page({
    */
   onLoad: function (options) {
     var that = this
-    that.get_order()
+    that.Iamcatcher()
+  },
+  /* 把本机的studentID传入，确定我有咩有提交代取员申请 */
+  Iamcatcher() {
+    var that = this
+    wx.request({
+      url: 'http://127.0.0.1:8000/express/ifapply', //仅为示例，并非真实的接口地址
+      data: {
+        studentId: wx.getStorageSync('studentId'),
+      },
+      method: "POST",
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' // 默认值
+      },
+      success(res) {
+        console.log(res.data.length);
+        if (res.data.length == 0) {
+          console.log("我还没有注册成为代取员");
+          /* 下面是申请板块展示 */
+        }
+      }
+    })
   },
   get_order() {
     var that = this
