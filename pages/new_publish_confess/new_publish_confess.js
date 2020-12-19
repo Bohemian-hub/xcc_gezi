46996e9a3ba0e1b20402924cfd0201cc50571be7
Bohemian-hub <dongshangwl@gmail.com>
2020-12-20 00:53:31 +0800

    做好了新表白的数据库入库

--- pages/new_publish_confess/new_publish_confess.js
+++ pages/new_publish_confess/new_publish_confess.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-16 22:19:42
- * @LastEditTime: 2020-12-18 23:50:28
+ * @LastEditTime: 2020-12-20 00:50:01
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /miniprogram-5/pages/new_publish_confess/new_publish_confess.js
@@ -27,6 +27,7 @@ Page({
     ifquestion4show: 'none',
     bottom_text: '重新填写',
     bywho: '',
+    which_college: '',
     information_college: ['计算机科学与技术', '电子科学与技术', '电子信息工程'],    //信息技术学院
     language_college: ['英语', '翻译', '商务英语'],         //外国语学院
     civil_college: ['土木工程', '工程管理', '水利水电工程', '给排水科学与工程'],          //土木与水利工程学院
@@ -107,6 +108,13 @@ Page({
       delay: 0,
       transformOrigin: '50% 50% 0'
     })
+    this.animation5 = wx.createAnimation({
+      duration: 4000,
+      timingFunction: 'ease',
+      delay: 0,
+      transformOrigin: '50% 50% 0'
+    })
+
     this.animation.scale(2, 2).opacity(1).step()
     this.setData({ animation: this.animation.export() })
   },
@@ -153,9 +161,10 @@ Page({
   college(e) {
     /* 让这个东西显示上面的消失 */
     this.animation2.scale(.5, .5).opacity(0).step()
-    this.setData({ animation2: this.animation2.export(), college: e.currentTarget.dataset.college, })
+    this.setData({ animation2: this.animation2.export() })
     console.log(this.data.college);
     this.setData({
+      which_college: e.currentTarget.dataset.college,
       college: this.data[e.currentTarget.dataset.college],
     })
     setTimeout(() => {
@@ -188,6 +197,8 @@ Page({
     setTimeout(() => {
       this.animation4.scale(2, 2).opacity(1).step()
       this.setData({ animation4: this.animation4.export() })
+      this.animation5.opacity(1).step()
+      this.setData({ animation5: this.animation5.export() })
     }, 1200);
   },
 
@@ -195,5 +206,97 @@ Page({
     wx.redirectTo({
       url: '../new_confesswall/new_confesswall',
     })
+  },
+  getInputValue1(e) {
+    console.log(e.detail)
+    this.setData({
+      confess_name: e.detail.value
+    })
+  },
+  getInputValue2(e) {
+    console.log(e.detail)
+    this.setData({
+      confess_content: e.detail.value
+    })
+  },
+  tijiaobiaobai(ifname) {
+    if (this.data.confess_name && this.data.confess_content) {
+      console.log("匿名提交")
+      console.log(this.data.sex);
+      console.log(this.data.which_college);
+      console.log(this.data.major);
+      console.log(this.data.confess_name);
+      console.log(this.data.confess_content);
+      /* 加载时间日期 */
+      var myDate = new Date();
+      var year = myDate.getFullYear()
+      var month = myDate.getMonth() + 1
+      var date = myDate.getDate()
+      var h = myDate.getHours();       //获取当前小时数(0-23)
+      var m = myDate.getMinutes();     //获取当前分钟数(0-59)
+      var s = myDate.getSeconds()     //获取当前分钟数(0-59)
+      if (month < 10) {
+        month = "0" + String(month)
+      }
+      if (date < 10) {
+        date = "0" + String(date)
+      }
+      if (s < 10) {
+        s = "0" + String(s)
+      }
+      if (h < 10) {
+        h = "0" + String(h)
+      }
+      if (m < 10) {
+        m = "0" + String(m)
+      }
+      var time = year + '-' + month + "-" + date + " " + h + ":" + m + ':' + s;
+      console.log(time);
+      wx.request({
+        url: 'http://127.0.0.1:8000/confess/add_new_confess', //仅为示例，并非真实的接口地址
+        data: {
+          confess_name: this.data.confess_name,
+          sex: this.data.sex,
+          which_college: this.data.which_college,
+          major: this.data.major,
+          confess_content: this.data.confess_content,
+          realname: wx.getStorageSync('name'),
+          studentId: wx.getStorageSync('studentId'),
+          name: ifname,
+          time: time
+        },
+        method: "POST",
+        header: {
+          'content-type': 'application/x-www-form-urlencoded' // 默认值
+        },
+        success: (result) => {
+          console.log(result.data.loginnum);
+          /* 现在已经成功的将表白信息存储到了数据库中，还少了一个表白时日期哦 */
+          if (result.data.loginnum == 200) {
+            /* 我准备做一个动画，模拟信飞出去，然后做一个提示框 ，显示信的去向 */
+            this.animation5.translate(300, -300).scale(.5, .5).opacity(0).step()
+            this.setData({ animation5: this.animation5.export() })
+          }
+        },
+      });
+
+
+
+
+    } else {
+      console.log("请完善")
+    }
+
+
+  },
+  /* 这是匿名提交 */
+  nonamesubmit() {
+    this.tijiaobiaobai("匿名")
+  },
+
+  /* 这不是匿名提交 */
+  confesssubmit() {
+    this.tijiaobiaobai(wx.getStorageSync('name'))
+
   }
 })
