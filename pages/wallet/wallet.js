b51c0d3cf503ca7a07a657b37b22e25898143102
Bohemian-hub <dongshangwl@gmail.com>
2021-06-05 08:46:59 +0800

    基本实现了钱包！

---
+++ pages/wallet/wallet.js
@@ -0,0 +1,30 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-04 14:57:54
+ * @LastEditTime: 2021-06-04 18:26:39
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /miniprogram-5/pages/wallet/wallet.js
+ */
+// pages/wallet/wallet.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  tixian(e) {
+    /* 提出提现选项；要求输入支付宝账号、 */
+    console.log(e.target.id);
+  }
+})
