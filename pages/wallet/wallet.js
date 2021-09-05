f9a9ef4914068feb6fe02bb95dd14f9debdf59c9
Bohemian-hub <dongshangwl@gmail.com>
2021-09-05 22:05:50 +0800

    完善了提现相关的炒作机制

--- pages/wallet/wallet.js
+++ pages/wallet/wallet.js
@@ -58,20 +58,6 @@ Page({
   },
   tixian(e) {
     /* 判断当前是否可以提现，如果有未到账的资金，就不能提现 */
-    if (this.data.wallet_nums2 != '0.00' || this.data.get_input_money != '') {
-      wx.showModal({
-        title: '提示',
-        content: '您当前有一笔提现未到账，暂不可发起二次提现。',
-        showCancel: false,
-        confirmText: '确定',
-        confirmColor: '#3CC51F',
-        success: (result) => {
-          if (result.confirm) {
-
-          }
-        },
-      });
-    } else {
       /* 提出提现选项；要求输入支付宝账号、 */
       console.log(e);
       if (e.target.dataset.classify == 'alipay') {
@@ -86,20 +72,22 @@ Page({
       this.setData({
         show_deposit: !this.data.show_deposit
       })
-    }
+    
 
   },
-  get_input_name(e) {
-    console.log(e.detail.value);
-    this.setData({
-      get_input_name: e.detail.value
-    })
-  },
-  get_input_account(e) {
-    console.log(e.detail.value);
-    this.setData({
-      get_input_account: e.detail.value
-    })
+  yuantofen(val) {
+    var s = val.split(".")
+    var yuan = parseInt(s[0]) * 100;
+    var fen = 0;
+    if (s.length > 1) {
+      let fenVal = (parseInt(s[1].substr(0, 1))) * 10;
+      fen += fenVal;
+      if (s[1].length > 1) {
+        fen += parseInt(s[1].substr(1, 1));
+      }
+    }
+    var fee = yuan + fen;
+    return fee;
   },
   get_input_money(e) {
     console.log(e.detail.value);
@@ -109,52 +97,88 @@ Page({
   },
   centain_withdraw() {
     var that = this
-    console.log(this.data.get_input_name);
-    console.log(this.data.get_input_account);
-    console.log(this.data.get_input_money);
+    if (Number(that.data.get_input_money)<1||Number(that.data.get_input_money)>30) {
+      wx.showModal({
+        title: '提示',
+        content: '请输入提现金额1-30元。',
+        showCancel: false,
+        confirmText: '确定',
+        confirmColor: '#3CC51F',
+        success: (result) => {
+          if (result.confirm) {
+
+          }
+        },
+      });
+    }else{
+
+
+
+    console.log("提现金额（yuan）");
+    console.log((Number(that.data.get_input_money)).toFixed(2));
+    console.log("提现金额（fen）");
+    console.log(that.yuantofen((Number(that.data.get_input_money)).toFixed(2)));
     /*     wx.showLoading({
           title: '申请中...',
           mask: true,
         }); */
     /* 现在发送一个后端请求！！！ */
-    wx.request({
-      url: 'https://www.xiyuangezi.cn/express/withdraw',
-      data: {
-        get_input_name: that.data.get_input_name,
-        get_input_account: that.data.get_input_account,
-        get_input_money: that.data.get_input_money,
-        deposit_way: that.data.classify,
-        studentId: wx.getStorageSync('studentId'),
-      },
-      header: {
-        "content-type": "application/x-www-form-urlencoded"		//使用POST方法要带上这个header
-      },
-      method: "POST",
-      success: res => {
-        console.log(res.data.loginnum);
-        if (res.data.loginnum == 200) {
-          /* 第一步关闭提现窗口。 */
-          this.setData({
-            show_deposit: !this.data.show_deposit
-          })
-          /* 展示提示框 */
-          wx.showModal({
-            title: '提示',
-            content: '提现已成功发起，预计24小时内到账！',
-            showCancel: false,
-            confirmText: '确定',
-            confirmColor: '#3CC51F',
-            success: (result) => {
-              if (result.confirm) {
-
-              }
-            },
-          });
-
-        }
+    wx.login({
+      success(res) {
+        wx.request({
+          url: 'https://www.xiyuangezi.cn/express/withdraw',
+          data: {
+            code: res.code,
+            get_input_money: that.yuantofen((Number(that.data.get_input_money)).toFixed(2)),
+            deposit_way: that.data.classify,
+            studentId: wx.getStorageSync('studentId'),
+            name: wx.getStorageSync('name'),
+          },
+          header: {
+            "content-type": "application/x-www-form-urlencoded"		//使用POST方法要带上这个header
+          },
+          method: "POST",
+          success: res => {
+            console.log(res.data.statusnum);
+            if (res.data.statusnum == 200) {
+              /* 第一步关闭提现窗口。 */
+              that.setData({
+                show_deposit: !that.data.show_deposit
+              })
+              /* 展示提示框 */
+              wx.showModal({
+                title: '提示',
+                content: '提现已成功发起，预计五分钟内到账！',
+                showCancel: false,
+                confirmText: '确定',
+                confirmColor: '#3CC51F',
+                success: (result) => {
+                  if (result.confirm) {
+    
+                  }
+                },
+              });
+    
+            }else{
+              wx.showModal({
+                title: '提示',
+                content: '提现失败，请联系客服或稍后重试!',
+                showCancel: false,
+                confirmText: '确定',
+                confirmColor: '#3CC51F',
+                success: (result) => {
+                  if (result.confirm) {
+    
+                  }
+                },
+              });
+            }
+          }
+        })
       }
     })
 
+  }
   },
   all_withdraw() {
     console.log("allllll");
